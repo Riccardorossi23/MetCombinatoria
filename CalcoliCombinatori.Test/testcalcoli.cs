d9ab0b6229ceb8cd4d602d6bcaf41b6f1c9092c9
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-24 13:13:52 +0100

    Add project files.

---
+++ CalcoliCombinatori.Test/testcalcoli.cs
@@ -0,0 +1,42 @@
+using EquazioniLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcoliCombinatori.Test
+{
+    [TestClass]
+    public class testcalcoli
+    {
+        [TestMethod]
+        public void TestFattoriale1()
+        {
+            long n = 1;
+            long numero_attuale = CalcoliEquazioniClass.Fattoriale(n);
+            long risposta_aspettato = 1;
+            Assert.AreEqual(risposta_aspettato, numero_attuale);
+        }
+        [TestMethod]
+        public void TestFattoriale2()
+        {
+            long n = 0;
+            double numero_attuale = CalcoliEquazioniClass.Fattoriale(n);
+            long risposta_aspettato = 1;
+            Assert.AreEqual(risposta_aspettato, numero_attuale);
+        }
+        [TestMethod]
+        public void TestFattoriale3()
+        {
+            long n = 5;
+            double numero_attuale = CalcoliEquazioniClass.Fattoriale(n);
+            long risposta_aspettato = 120;
+            Assert.AreEqual(risposta_aspettato, numero_attuale);
+        }
+        [TestMethod]
+        public void TestFattoriale4()
+        {
+            long n = 10;
+            double numero_attuale = CalcoliEquazioniClass.Fattoriale(n);
+            long risposta_aspettato = 3628800;
+            Assert.AreEqual(risposta_aspettato, numero_attuale);
+        }
+    }
+}
---
+++ EquazioniLibrary/CalcoliEquazioniClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EquazioniLibrary
+{
+    public class CalcoliEquazioniClass
+    {
+        public static long Fattoriale(long n)
+        {
+            if (n == 0)
+            {
+                throw new Exception("il numero non deve essere negativo quindi non può essere calcolato");
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Fattoriale(n - 1);
+            }
+        }
+    }
+}
